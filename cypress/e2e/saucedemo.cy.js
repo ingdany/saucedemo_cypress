7b26c1b8c0b562743a48cd3666a0af1dc6a5e77e
Daniel Pérez <ing.dany@gmail.com>
2025-01-20 12:12:59 -0800

    Add fixture file

--- cypress/e2e/saucedemo.cy.js
+++ cypress/e2e/saucedemo.cy.js
@@ -5,11 +5,32 @@ import CheckoutPage from "../pages/CheckoutPage";
 import OverviewPage from "../pages/OverviewPage";
 import FinishPage from "../pages/FinishPage";
 
-describe("Sauce Demp E2E Test", () => {
-    it("Login ", () => {
-        LoginPage.visit();
-        LoginPage.login("standard_user", "secret_sauce");
+describe("Sauce Demo E2E Test", () => {
+    before(() => {
+        
+        cy.fixture('users.json').as("users");
+        // cy.fixture('users.json').then((users) => {
+            
+        //     // user = users[0].username;
+        //     // password = users[0].password;
+        //     // cy.log("User: " + user, "Password: " + password);
+            
+        // })
 
+        
+    })
+
+    it("Make a purchase of 2 items", () => {
+        let userName = "";
+        let password = "";
+        cy.get("@users").then((users) => {
+            userName = users.users[0].username;
+            password = users.users[0].password;
+            cy.log("User: " + userName, "Password: " + password);
+            LoginPage.visit();
+            LoginPage.login(userName, password);
+        })
+        
         InventoryPage.addProductToCart(0);
         InventoryPage.addProductToCart(1);
         InventoryPage.goToCart();
